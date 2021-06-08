1a18f50549897c5936b27f9e401b06dcf296176b
hannypher <hannypherneenah@gmail.com>
2021-06-08 15:39:50 +0300

    created register and sigin page

--- registration.js
+++ registration.js
@@ -19,6 +19,78 @@
       })
   })()
 
+  (function() {
+    ("form[name='login']").validate({
+      rules: {
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+        }
+      },
+      messages: {
+        email: "please enter a valid email address",
+
+        password: {
+          required: "please enter password",
+        }
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+
+  (function() {
+    ("form[fullName='registration']").validate({
+      rules: {
+        fullName: "required",
+        username: {
+          username: "required"
+        },
+        email: {
+          required: "true",
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 5
+        },
+        gender: {
+          required: true,
+          gender: true
+        },
+        country: {
+          required: true,
+          country: true
+        },
+        cityDistrict: {
+          required: true,
+          cityDistrict: true
+        }
+      },
+
+      messages: {
+        fullName: "please enter your full_name",
+        username: "please enter your username",
+        password: {
+          required: "please provide a password",
+        minlength: "your password must be atleast 5 characters long"
+        },
+        email: " please enter a valid email address",
+        gender: "please choose your gender",
+        country: "please select your country",
+        cityDistrict: "please choose your city_district"
+      },
+
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+
   //select the form
 //const registrationForm = document.querySelector('.registration-form');
 //const formControl = document.querySelector('.form-control');
